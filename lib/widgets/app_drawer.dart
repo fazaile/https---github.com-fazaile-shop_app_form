2e663004af3229ce5a06fe19804e99a1f2491fc5
fazaile <fazaile108@gmail.com>
2022-12-23 02:44:19 +0800

    init

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'package:shopp_app/screens/user_product_screen.dart';
+import 'package:shopp_app/widgets/user_product_item.dart';
+import '../screens/orders_screen.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text('Hello Friends!'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Shop'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Orders'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(OrdersScreen.routeName);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Manage Products'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(UserProductScreen.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
